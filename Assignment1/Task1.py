3a6b8b4522edb2339195b080b47e468016fc12fc
prashant4833o <prashant4833o@gmail.com>
2025-10-15 15:05:11 +0530

    initial commit

---
+++ Assignment1/Task1.py
@@ -0,0 +1,34 @@
+
+#! Task 1: Perform Basic Mathematical Operations
+#! Problem Statement: Write a Python program that does the following:
+#! 1.  Takes two numbers as input from the user.
+#! 2.  Performs the basic mathematical operations on these two numbers:
+#! o	Addition
+#! o	Subtraction
+#! o	Multiplication
+#! o	Division
+#! 3.  Displays the results of each operation on the screen.
+
+"""Program to perform basic mathematical operations on two numbers."""
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+print("Addition of two numbers is: ",addition)
+print("Subtraction of two numbers is: ",subtraction)
+print("Multiplication of two numbers is: ",multiplication)
+print("Division of two numbers is: ",division)
+#~ Sample Input:
+#~ Enter first number: 10
+#~ Enter second number: 5
+#~ Sample Output:
+#~ Addition of two numbers is: 15
+#~ Subtraction of two numbers is: 5
+#~ Multiplication of two numbers is: 50
+#~ Division of two numbers is: 2.0
+#~ Explanation:
+#~ The program takes two numbers as input from the user and performs addition, subtraction, multiplication, and division on these numbers. It then displays the results of each operation on the screen.
+
+
---
+++ Assignment1/Task2.py
@@ -0,0 +1,18 @@
+#! Task 2: Create a Personalized Greeting
+#! Problem Statement: Write a Python program that:
+#! 1.  Takes a user's first name and last name as input.
+#! 2.  Concatenates the first name and last name into a full name.
+#! 3.  Prints a personalized greeting message using the full name
+
+"""Program to create a personalized greeting."""
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+full_name=first_name+" "+last_name
+print("Hello",full_name,"! Welcome to Python programming.")
+#~ Sample Input:
+#~ Enter your first name: John
+#~ Enter your last name: Doe
+#~ Sample Output:
+#~ Hello John Doe! Welcome to Python programming.
+#~ Explanation:
+#~ The program takes the user's first name and last name as input and concatenates them into a full name. It then prints a personalized greeting message using the full name.
